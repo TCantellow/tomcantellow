92daef2cf150e48936661358b34a7d1340bd17b0
TCantellow <146112015+TCantellow@users.noreply.github.com>
2024-11-06 16:10:02 +0100

    added projects

--- assets/scripts.js
+++ assets/scripts.js
@@ -8,3 +8,21 @@ function loadHeader() {
             document.querySelector(".toggle-button").onclick = toggleMode;
         });
 }
+
+let currentIndex = 0; // Keep track of the current index of carousel items
+const items = document.querySelectorAll('.carousel-item'); // Select all carousel items
+
+// Function to change the slide
+function changeSlide(direction) {
+    // Hide the current slide by removing the 'active' class
+    items[currentIndex].classList.remove('active');
+
+    // Update the index for the new slide
+    currentIndex = (currentIndex + direction + items.length) % items.length;
+
+    // Show the new slide by adding the 'active' class
+    items[currentIndex].classList.add('active');
+}
+
+// Initialize the first slide as active
+items[currentIndex].classList.add('active');
